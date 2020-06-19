419f6992b2fdffa3a603a4d6aa3602ba22f34121
Vatsal Manot <vatsal.manot@yahoo.com>
2020-06-19 20:31:53 +0530

    Update package

--- Sources/FirebaseSPM.swift
+++ Sources/FirebaseSPM.swift
@@ -2,89 +2,56 @@
 // Copyright (c) Vatsal Manot
 //
 
-#if canImport(Firebase)
-
-import Dispatch
-import Firebase
-import FirebaseSPM
 import Foundation
 
-extension FirebaseSPM {
-    public static func initialize() {
-        DatabaseType = Database.self
-        DataSnapshotType = DataSnapshot.self
-    }
+public struct FirebaseSPM {
+    public static var DatabaseType: DatabaseProtocol.Type!
+    public static var DataSnapshotType: DataSnapshotProtocol.Type!
 }
 
-extension Database: DatabaseProtocol {
-    public var _app: FirebaseAppProtocol? {
-        app
-    }
-    
-    public static func database() -> DatabaseProtocol {
-        self.database() as Database
-    }
-    
-    public static func database(url: String) -> DatabaseProtocol? {
-        database(url: url) as Database
-    }
-    
-    public static func database(app: FirebaseAppProtocol, url: String) -> DatabaseProtocol {
-        database(app: app as! FirebaseApp, url: url)
-    }
-    
-    public static func database(app: FirebaseAppProtocol) -> DatabaseProtocol {
-        database(app: app as! FirebaseApp) as DatabaseProtocol
-    }
-    
-    public func reference() -> DatabaseReferenceProtocol {
-        reference() as DatabaseReference
-    }
-    
-    public func reference(withPath path: String) -> DatabaseReferenceProtocol{
-        reference(withPath: path) as DatabaseReference
-    }
-    
-    public func reference(fromURL databaseUrl: String) -> DatabaseReferenceProtocol {
-        reference(fromURL: databaseUrl) as DatabaseReference
-    }
-}
-
-extension DatabaseReference: DatabaseReferenceProtocol {
-    public func child(_ pathString: String) -> DatabaseReferenceProtocol {
-        child(pathString) as DatabaseReference
-    }
-    
-    public func observe(_ eventType: _DataEventType, with block: @escaping (DataSnapshotProtocol) -> ()) {
-        observe(DataEventType(eventType)) {
-            block($0)
-        }
-    }
-}
-
-extension DataSnapshot: DataSnapshotProtocol {
+public protocol FirebaseAppProtocol: class {
     
 }
 
-extension FirebaseApp: FirebaseAppProtocol {
+public protocol DatabaseProtocol: class {
+    static func database() -> DatabaseProtocol
+    static func database(url: String) -> DatabaseProtocol?
+    static func database(app: FirebaseAppProtocol, url: String) -> DatabaseProtocol
+    static func database(app: FirebaseAppProtocol) -> DatabaseProtocol
     
+    static func setLoggingEnabled(_ enabled: Bool)
+    static func sdkVersion() -> String
+    
+    var _app: FirebaseAppProtocol? { get }
+    var isPersistenceEnabled: Bool { get set }
+    var persistenceCacheSizeBytes: UInt { get set }
+    var callbackQueue: DispatchQueue { get set }
+    
+    func reference() -> DatabaseReferenceProtocol
+    func reference(withPath _: String) -> DatabaseReferenceProtocol
+    func reference(fromURL _: String) -> DatabaseReferenceProtocol
+    
+    func purgeOutstandingWrites()
+    func goOffline()
+    func goOnline()
 }
 
-extension DataEventType {
-    public init(_ eventType: _DataEventType) {
-        switch eventType {
-            case .childAdded:
-                self = .childAdded
-            case .childRemoved:
-                self = .childRemoved
-            case .childChanged:
-                self = .childChanged
-            case .childMoved:
-                self = .childMoved
-            case .value:
-                self = .value
-        }
-    }
+public protocol DatabaseReferenceProtocol: class {
+    func child(_: String) -> DatabaseReferenceProtocol
+    func observe(_: _DataEventType, with _: @escaping (DataSnapshotProtocol) -> ()) 
+    func removeAllObservers()
 }
 
-#endif
+public protocol DataSnapshotProtocol: class {
+    var value: Any? { get }
+    
+    func exists() -> Bool
+}
+
+public enum _DataEventType {
+    case childAdded
+    case childRemoved
+    case childChanged
+    case childMoved
+    case value
+}
